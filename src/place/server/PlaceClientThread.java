b3c5ac0a072f1906986da79a4d989260f0d2081f
Caitlin Kimbrell <cbk3451@g.rit.edu>
2017-12-07 16:05:15 -0500

    boi i dunno whats happening

---
+++ src/place/server/PlaceClientThread.java
@@ -0,0 +1,47 @@
+package place.server;
+
+import jdk.nashorn.internal.runtime.ECMAException;
+import place.PlaceBoard;
+import place.PlaceException;
+
+import java.io.BufferedWriter;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class PlaceClientThread implements Runnable, Closeable {
+    private Socket sock;
+    private String username;
+    private PrintStream printer;
+    private Scanner scanner;
+    private BufferedWriter writer;
+    private PlaceBoard board;
+    public PlaceClientThread(String hostname, int port, PlaceBoard model){
+
+        try{
+            this.sock = new Socket( hostname, port );
+            this.scanner = new Scanner( sock.getInputStream() );
+            this.printer = new PrintStream( sock.getOutputStream() );
+            this.board = model;
+            printer.print( "Connected to server " + this.sock );
+            Thread netThread = new Thread( () -> this.run() );
+            netThread.start();
+        }catch(Exception e){
+
+        }
+    }
+    public void run(){
+        try{
+
+        }catch(Exception e){}
+    }
+
+    public void close(){
+        try{
+            this.sock.close();
+        }catch(Exception e){
+        }
+    }
+}
---
+++ src/place/server/PlaceServer.java
@@ -0,0 +1,79 @@
+package place.server;
+
+
+import place.PlaceBoard;
+import place.PlaceException;
+import place.TestClient;
+import place.network.PlaceRequest;
+import place.server.PlaceClientThread;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.Map;
+
+public class PlaceServer implements Closeable{
+    private ServerSocket server;
+    private static int dim;
+    private Map<String, ObjectOutputStream> clientMap;
+    private PlaceBoard board;
+    public PlaceServer(int port) throws PlaceException {
+        try {
+            this.server = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new PlaceException(e);
+        }
+
+    }
+    public synchronized void sendBoard(){
+        Iterator it = clientMap.keySet().iterator();
+        while(it.hasNext()){
+            try{
+                clientMap.get(it.next()).writeObject(new PlaceRequest<>(PlaceRequest.RequestType.BOARD, board) );
+            }catch(Exception e){
+
+            }
+
+        }
+    }
+    public void run(){
+        board = new PlaceBoard(dim);
+        try{
+            Socket clientSocket = server.accept();
+            try {
+                TestClient client = new TestClient(clientSocket);
+                PlaceRequest<PlaceBoard> boardReq = new PlaceRequest<>(PlaceRequest.RequestType.BOARD, board);
+                ObjectInputStream in;
+                PlaceRequest<?> req = new PlaceRequest<?> (in.readObject());
+            }catch(Exception e){
+
+            }
+
+        }catch(Exception e){
+
+        }
+    }
+    @Override
+    public void close() throws IOException {
+        try {
+            this.server.close();
+        } catch (IOException ioe) {
+        }
+    }
+    public static void main(String[] args) throws PlaceException {
+        if (args.length != 2) {
+            System.exit(1);
+        }
+        dim = Integer.parseInt(args[1]);
+        try (PlaceServer server = new PlaceServer(Integer.parseInt(args[0]))) {
+            server.run();
+        } catch (Exception e) {
+            System.err.println("Failed to start server!");
+            e.printStackTrace();
+        }
+    }
+}
