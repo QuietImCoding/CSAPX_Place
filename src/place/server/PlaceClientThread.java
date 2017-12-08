f6244f9f4efa7afcd1ec4db3002dfa635cad1ef9
Caitlin Kimbrell <cbk3451@g.rit.edu>
2017-12-08 14:33:16 -0500

    still gotta do things but it compitlejsljel

--- src/place/server/PlaceClientThread.java
+++ src/place/server/PlaceClientThread.java
@@ -3,44 +3,75 @@ package place.server;
 import jdk.nashorn.internal.runtime.ECMAException;
 import place.PlaceBoard;
 import place.PlaceException;
+import place.PlaceTile;
+import place.network.PlaceRequest;
 
-import java.io.BufferedWriter;
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class PlaceClientThread implements Runnable, Closeable {
     private Socket sock;
     private String username;
-    private PrintStream printer;
-    private Scanner scanner;
-    private BufferedWriter writer;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
     private PlaceBoard board;
-    public PlaceClientThread(String hostname, int port, PlaceBoard model){
+//    public PlaceClientThread(String hostname, int port, PlaceBoard model){
+//
+//        try{
+//            this.sock = new Socket( hostname, port );
+//            this.scanner = new Scanner( sock.getInputStream() );
+//            this.printer = new PrintStream( sock.getOutputStream() );
+//            this.board = model;
+//            printer.print( "Connected to server " + this.sock );
+//            Thread netThread = new Thread( () -> this.run() );
+//            netThread.start();
+//        }catch(Exception e){
+//
+//        }
+//    }
 
+    public PlaceClientThread(Socket socket){
+        this.sock = sock;
+        try {
+            this.in = new ObjectInputStream(sock.getInputStream());
+            this.out = new ObjectOutputStream(sock.getOutputStream());
+        }
+        catch (IOException e) {
+        }
+    }
+
+    public void changed(PlaceTile tile){
+        PlaceRequest<PlaceTile> tileChange = new PlaceRequest<>(PlaceRequest.RequestType.CHANGE_TILE, tile);
         try{
-            this.sock = new Socket( hostname, port );
-            this.scanner = new Scanner( sock.getInputStream() );
-            this.printer = new PrintStream( sock.getOutputStream() );
-            this.board = model;
-            printer.print( "Connected to server " + this.sock );
-            Thread netThread = new Thread( () -> this.run() );
-            netThread.start();
-        }catch(Exception e){
+            out.writeObject(tileChange);
+        }catch(IOException e){
 
         }
     }
-    public void changed(){
-
-    }
     public void run(){
         try{
+            boolean listening = true;
+            while(listening){
+                PlaceRequest<?> req = (PlaceRequest<?>)in.readObject();
+                if(req.getType() == PlaceRequest.RequestType.LOGIN){
+                    username = (String)req.getData();
+                    login();
+                }
 
+
+            }
         }catch(Exception e){}
     }
-
+    public String getUserName(){
+        return username;
+    }
+    public void login(){
+        //ask server to log in
+    }
+    public void start(){
+        this.run();
+    }
     public void close(){
         try{
             this.sock.close();
